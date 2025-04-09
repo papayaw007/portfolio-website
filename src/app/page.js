5bef90af7e1ba643d988e1e9b630cf05f58cb89e
papayaw007 <papa.adu-asare@ashesi.edu.gh>
2025-04-09 20:01:59 +0000

    hero-section implementation

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div>
+      <Hero/>
       <p className="text-5xl">HelloWorld! Hi i am PY</p>
     </div>
   );
